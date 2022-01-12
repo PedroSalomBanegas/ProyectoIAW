6b06e7f3c6e8b96a1b823b448ea0384b245d84e0
PedroSalomBanegas <psalomb59@alumnes.santjosepobrer.es>
2022-01-12 20:22:46 +0100

    BlackJack Fundamentals

--- index.php
+++ index.php
@@ -1,3 +1,65 @@
+<?php 
+
+include('includes.php');
+include('bot.php');
+
+$gameDeck = $deck;
+$playerHand = [];
+$botHand = [];
+$turn = true;
+
+function drawCard() {
+    // Author: Pedro
+    global $gameDeck;
+
+    $randomCard = rand(0, count($gameDeck) - 1);
+    // echo count($gameDeck);
+    // echo "<br>";
+    // echo $gameDeck[$randomCard]['reference'] . " of " . $gameDeck[$randomCard]['type']; 
+    // echo "<br><br>";
+    
+    saveCards($gameDeck[$randomCard]);
+
+    unset($gameDeck[$randomCard]);
+    $gameDeck = array_values($gameDeck);
+    // print_r($gameDeck[$randomCard]); 
+    // print_r($gameDeck);
+}
+
+function begin() {
+    //Author: Jaime
+    global $turn;
+        for ($i=0; $i < 4 ; $i++) { 
+            if ($i < 2) {
+                drawCard();
+            } else {
+                $turn = false;
+                drawCard();
+            }
+        }
+        $turn = true;
+    }
+
+// begin();
+
+function saveCards($card) {
+    //Author: Jaime
+    global $playerHand;
+    global $botHand;
+    global $turn;
+    
+    if ($turn) {
+        $playerHand[] = $card;
+    } else {
+        $botHand[] = $card;
+    }
+    echo "<h2>Player</h2>";
+    print_r($playerHand);
+    echo "<h2>Bot</h2>";
+    print_r($botHand);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +69,16 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Prueba Sync</h1>
+    <h1>BlackJack</h1>
+    <button id="draw">Draw Card</button>
 </body>
+
+<script>
+    const drawButton = document.getElementById("draw");
+
+    drawButton.addEventListener("click", () => {
+        <?php drawCard() ?>
+    });
+</script>
+
 </html>
