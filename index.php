df3783244244b27376387e47183665163a1d4702
PedroSalomBanegas <psalomb59@alumnes.santjosepobrer.es>
2022-01-14 17:09:18 +0100

    Update cookies & Sessions

--- index.php
+++ index.php
@@ -1,17 +1,34 @@
 <?php 
 
+// ------------------------          
+//          NOTAS
+// No se van borrar las cookies si se utilizará alguna función que las genere (ej: begin, hit, etc...)
+// ¿Solución?
+// Para evitar que alguien no ejecute la web con una acción directa y rompa la ejecución del juego, poner una cookie de control que se genere con ?action=new??
+// ------------------------
+
+session_start();
+
 include('includes.php');
 include('bot.php');
 
-$gameDeck = $deck;
+// -- EXPERIMENTAL!! --
+if(isset($_GET['action']) && isset($_SESSION['token'])){
+    if ($_GET['action'] == "new") {
+        $gameDeck = $deck;
+    } elseif (isset($_SESSION['status'])) {
+        $gameDeck = json_decode($_COOKIE['deck'], true);
+    }
+}
+// -- EXPERIMENTAL !! --
 
-if(isset($_COOKIE['playerHand'])){
+if(isset($_COOKIE['playerHand']) && isset($_SESSION['token'])){
     $playerHand = json_decode($_COOKIE['playerHand'], true);
 } else {
     $playerHand = [];
 }
 
-if(isset($_COOKIE['botHand'])){
+if(isset($_COOKIE['botHand']) && isset($_SESSION['token'])){
     $botHand = json_decode($_COOKIE['botHand'], true);
 } else {
     $botHand = [];
@@ -19,27 +36,65 @@ if(isset($_COOKIE['botHand'])){
 
 $turn = true;
 
+if(isset($_GET['action']) && isset($_SESSION['token'])){
+    $action = $_GET['action'];
+
+    if($action == "hit" && isset($_SESSION['status'])){
+        drawCardF();
+    } elseif ($action == "end" && isset($_SESSION['status'])) {
+        setcookie('deck', '', 0);
+        setcookie('playerHand', '', 0);
+        setcookie('botHand', '', 0);
+    } elseif ($action == "new" && !isset($_SESSION['status'])) {
+        begin();
+    }
+} elseif (isset($_GET['action']) && !isset($_SESSION['token']) && !isset($_SESSION['status'])) {
+    clearCookies();
+} else {
+    //First time enter 
+    clearCookies();
+    // setcookie('token', true, (time()+3600*24*30)); //HACERLO CON SESIONES!!!!
+    $_SESSION['token']=true;
+}
+
 function drawCard() {
     // Author: Pedro
     global $gameDeck;
 
     $randomCard = rand(0, count($gameDeck) - 1);
-    // echo count($gameDeck);
-    // echo "<br>";
-    // echo $gameDeck[$randomCard]['reference'] . " of " . $gameDeck[$randomCard]['type']; 
-    // echo "<br><br>";
-    
     saveCards($gameDeck[$randomCard]);
-
     unset($gameDeck[$randomCard]);
     $gameDeck = array_values($gameDeck);
-    // print_r($gameDeck[$randomCard]); 
-    // print_r($gameDeck);
+    setcookie("deck", json_encode($gameDeck), (time()+3600*24*30));
+}
+
+function drawCardF() {
+    // Author: Pedro
+    global $gameDeck;
+    global $playerHand;
+    global $botHand;
+
+    $randomCard = rand(0, count($gameDeck) - 1);
+    saveCards($gameDeck[$randomCard]);
+    unset($gameDeck[$randomCard]);
+    $gameDeck = array_values($gameDeck);
+    setcookie("deck", json_encode($gameDeck), (time()+3600*24*30));
+
+    echo "<h2>Player Hand</h2> <br>\n";
+    print_r($playerHand);
+    echo "<h2>Bot Hand</h2> <br>\n";
+    print_r($botHand);
 }
 
 function begin() {
     //Author: Jaime
+    setcookie('playerHand', '', 0);
+    setcookie('botHand', '', 0);
+
     global $turn;
+    global $playerHand;
+    global $botHand;
+
         for ($i=0; $i < 4 ; $i++) { 
             if ($i < 2) {
                 drawCard();
@@ -49,9 +104,12 @@ function begin() {
             }
         }
         $turn = true;
-    }
 
-// begin();
+        echo "<h2>Player Hand</h2> <br>\n";
+        print_r($playerHand);
+        echo "<h2>Bot Hand</h2> <br>\n";
+        print_r($botHand);
+    }
 
 function saveCards($card) {
     //Author: Jaime
@@ -66,10 +124,6 @@ function saveCards($card) {
         $botHand[] = $card;
         setcookie("botHand", json_encode($botHand), (time()+3600*24*30));
     }
-    echo "<h2>Player</h2>";
-    print_r($playerHand);
-    echo "<h2>Bot</h2>";
-    print_r($botHand);
 }
 
 function countCards($hand) {
@@ -85,15 +139,20 @@ function countCards($hand) {
     return $Total;
 }
 
-begin();
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
+function clearCookies(){
+    setcookie('deck', '', 0);
+    setcookie('playerHand', '', 0);
+    setcookie('botHand', '', 0);
+}
 
-echo countCards($playerHand);
-echo "<br>";
-echo countCards($botHand);
+// echo "<br>";
+// echo "<br>";
+// echo "<br>";
+// echo "<br>";
+
+// echo countCards($playerHand);
+// echo "<br>";
+// echo countCards($botHand);
 
 ?>
 
@@ -107,6 +166,32 @@ echo countCards($botHand);
 </head>
 <body>
     <h1>BlackJack</h1>
-    <a href="?action=hit">Hit</a>
+    <?php 
+    if(isset($_GET['action']) && isset($_SESSION['token'])){
+        $action = $_GET['action'];
+    
+        if($action == "hit" ){
+            echo '<a href="?action=hit">Hit</a>';
+            echo '<br>';
+            echo '<a href="?action=end">End Game</a>';
+        } elseif ($action == "end") {
+            echo '<a href="?action=new">New Game</a>';
+        } elseif ($action == "new" && !isset($_SESSION['status'])) {
+            echo '<a href="?action=hit">Hit</a>';
+            echo '<br>';
+            echo '<a href="?action=end">End Game</a>';
+            $_SESSION['status']=true; //Generate here for security
+        } else {
+            echo '<h2>It seems that something has gone wrong...</h2>';
+            echo '<a href="index.php">Return</a>';
+        }
+    
+    } elseif(isset($_GET['action']) && !isset($_SESSION['token']) && !isset($_SESSION['status'])) { 
+        echo '<h2>It seems that something has gone wrong...</h2>';
+        echo '<a href="index.php">Return</a>';
+    } else {
+        echo '<a href="?action=new">New Game</a>';
+    }
+    ?>
 </body>
 </html>
