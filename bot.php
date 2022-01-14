b3e2dab2f8d92e17e412eba32522545531598021
JaimePons <jponsf30@alumnes.santjosepobrer.es>
2022-01-14 17:32:02 +0100

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,3 +1,14 @@
 <?php
-    
+   function bot() {
+       $totalBot = countCards($botHand);
+       if ($totalBot = 21 ) {
+           return $totalBot;
+       } else {
+           while ($totalBot < 21) {
+               drawcard()
+               $totalBot = countCards($botHand);
+           }
+           return $totalBot;
+       }
+   }
 ?>
