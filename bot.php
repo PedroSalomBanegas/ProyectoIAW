f913f8f3d6cd713c87b359ef8e046006e0e6914e
JaimePons <jponsf30@alumnes.santjosepobrer.es>
2022-01-17 16:00:52 +0100

    Update bot() and endgame()

--- bot.php
+++ bot.php
@@ -1,15 +1,43 @@
 <?php
    function bot() {
        global $botHand;
+       global $turn;
        $totalBot = countCards($botHand);
+       //print_r($botHand);
        if ($totalBot == 21 ) {
-           return $totalBot;
        } else {
+           $turn = false;
            while ($totalBot < 21) {
-               drawcard();
+               drawCard();
                $totalBot = countCards($botHand);
            }
-           return $totalBot;
+           $turn = true;
+       }
+   }
+
+   function endgame() {
+       global $playerHand;
+       global $botHand;
+
+       $finalPlayer = countCards($playerHand);
+       $finalBot = countCards($botHand);
+
+       if ($finalPlayer > 21) {
+           if ($totalBot > 21) {
+               return "Tie";
+           } else {
+               return "Bot Wins";
+           }
+       } else {
+           if ($finalBot > 21) {
+               return "Player Wins";
+           } else {
+               if ($finalPlayer > $finalBot) {
+                   return "PlayerWins";
+               } else {
+                   return "Bot Wins";
+               }
+           }
        }
    }
 ?>
